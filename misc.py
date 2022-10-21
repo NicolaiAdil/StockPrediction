3c50542fc4c2567dcbdd4f6ba0050ea4d3fbea6d
NicolaiAdil <NicolaiAdil.Aatif@gmail.com>
2022-10-21 10:57:04 +0200

    Created a gigignore file, and some functionality for csv

--- misc.py
+++
@@ -1,7 +0,0 @@
-def getData(csvFile : str) -> tuple(list, list):
-    time = []
-    close = []
-    with open(csvFile, 'r') as f:
-        for line in f:
-            data.append(line.strip().split(','))
-    return data
