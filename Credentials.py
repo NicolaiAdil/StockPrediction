5391fff03600f3e9249064ee9741ab6f6f94a8f8
NicolaiAdil <nicolaiadil.aatif@gmail.com>
2022-11-11 12:06:32 +0100

    misc

--- Credentials.py
+++
@@ -1,6 +0,0 @@
-
-
-storageaccounturl= "https://stockpredictio5203052435.blob.core.windows.net"
-storageaccountkey= "IEZDxzg3RoO8wxbWphz70pwGQbpIk+0UWky0iC3YvBXAvBinrKIZpI3wuzNK3vqzCueCnvKHVzx/+AStF2KCEQ=="
-containername= "predictionfiles"
-blobname= "ds.csv"
